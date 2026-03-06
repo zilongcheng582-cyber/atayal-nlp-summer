8be6501ac4bd2be2866a07acb6f5daae44a63cf1
DragonEatsOrange <zilongcheng582@gmail.com>
2026-03-06 20:17:57 +0800

    Add Atayal LSTM model class

--- src/model.py
+++ src/model.py
@@ -1,1 +1,16 @@
-#Atayal LSTM Language Model
+# Atayal LSTM Language Model
+import torch.nn as nn
+
+
+class AtayalLSTM(nn.Module):
+    def __init__(self, vocab_size, embed_dim, hidden_size, num_layers):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+        self.lstm = nn.LSTM(embed_dim, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x, _ = self.lstm(x)
+        x = self.fc(x)
+        return x
